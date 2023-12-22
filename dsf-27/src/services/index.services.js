1dc8531d99496b0fb6ee3a2b6c1e8a0fcd1a2d2a
eltipodev <eltipodev@gmail.com>
2023-12-22 18:33:38 -0300

    ok_final_v4

--- dsf-27/src/services/index.services.js
+++ dsf-27/src/services/index.services.js
@@ -2,9 +2,8 @@ import { Carts, Products, Users } from "../DAL/daos/factory.js";
 import CartsRepository from "./carts.service.js";
 import ProductsRepository from "./products.service.js";
 import UsersRepository from "./users.service.js";
-// import TicketsRepository from "./ticketsRepository.js";
 
 export const usersService = new UsersRepository(new Users());
 export const productsService = new ProductsRepository(new Products());
 export const cartsService = new CartsRepository(new Carts());
-// export const ticketsService = new TicketsRepository(new Tickets());
+
